d9dddeb163af26ef9b08f74221df1d4b30064037
seu-usersaneme <marianeni@yahoo.com.br>
2024-01-23 18:04:20 -0300

    scss dialog e alteracao no index para deploy

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -30,6 +30,20 @@ export class ProjectsComponent {
         }
       ]
     },
+    {
+      src: 'assets/img/projects/vfull.png',
+      alt: '',
+      title: 'Vida Fullstack2',
+      width: '100px',
+      height: '51px',
+      description: '<p>Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular, Front-end, JavaScript, Node.js e TypeScript.</p> <p>Desvende os segredos por trás da construção de interfaces modernas, aprimore suas habilidades de programação front-end quanto back-end e mergulhe em conceitos avançados de JavaScript e TypeScript.</p>',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'http://vidafullstack.com.br',
+        }
+      ]
+    },
   ]);
 
   public openDialog(data: IProjects) {
